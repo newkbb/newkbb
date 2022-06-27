5f53f9df300bbaedebb923c2a634fee852f1f04f
Eleven <597292532@qq.com>
2022-06-27 13:11:40 +0800

    添加了school的entity和resolver，用于查询与创建

---
+++ src/resolvers/shcool.resolver.ts
@@ -0,0 +1,29 @@
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql'
+import { getMongoRepository } from 'typeorm'
+import { ForbiddenError } from 'apollo-server-core'
+
+import { School } from '@entities'
+import { CreateScoolInput } from '../generator/graphql.schema'
+
+@Resolver('School')
+export class SchoolResolver {
+	@Query()
+	async schools(): Promise<School[]> {
+		return getMongoRepository(School).find({
+			cache: true
+		})
+	}
+
+	@Mutation()
+	async createCity(@Args('input') input: CreateScoolInput): Promise<School> {
+		const { name } = input
+
+		const schools = await getMongoRepository(School).findOne({ name })
+
+		if (schools) {
+			throw new ForbiddenError('City already existed.')
+		}
+
+		return await getMongoRepository(School).save(new School({ ...input }))
+	}
+}
